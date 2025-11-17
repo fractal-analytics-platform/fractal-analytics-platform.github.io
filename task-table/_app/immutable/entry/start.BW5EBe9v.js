1abae9ef5ff7b76947c5a316da6b05e72305f94c
tcompa <tcompa@users.noreply.github.com>
2025-11-17 16:43:52 +0000

    deploy: c82689d21d0c54c57b108ee1e2827bc2e4d88106

---
+++ task-table/_app/immutable/entry/start.BW5EBe9v.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/CHZtM6zs.js";export{o as load_css,r as start};
--- task-table/_app/immutable/entry/start.BcboQO8R.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BhmRedRC.js";export{o as load_css,r as start};
